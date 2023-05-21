25af2c90577a0a7ffc19790bd7fe710e440431b4
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-05-21 11:58:49 -0400

    feat

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,24 @@
 import Footer from "../../layout/Footer";
 import Navbar from "../../layout/Navbar";
 import savings from "../../image/savings.svg";
-import "../../layout/container.css";
 import "../../pages/Home/style.css";
 import LinkButton from "../../layout/LinkButton";
+import Container from "../../layout/Container";
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <div className="container_center">
-          <section className="home_container">
-            <h1>
-              Bem Vindo ao <span>Costs</span>
-            </h1>
-            <p>Comece a gerenciar os seus projetos agora mesmo</p>
-            <LinkButton to="newproject" text="Criar Projeto" />
-            <img src={savings} alt="costs" />
-          </section>
-        </div>
-      </div>
+      <Container customClass="min-height">
+        <section className="home_container">
+          <h1>
+            Bem Vindo ao <span>Costs</span>
+          </h1>
+          <p>Comece a gerenciar os seus projetos agora mesmo</p>
+          <LinkButton to="newproject" text="Criar Projeto" />
+          <img src={savings} alt="costs" />
+        </section>
+      </Container>
       <Footer />
     </div>
   );
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,14 +1,64 @@
-import Footer from "../../layout/Footer";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "../../layout/Navbar";
+import "../../layout/Message/Message.module.css";
+import "./style.css";
+import Message from "../../layout/Message";
+import LinkButton from "../../layout/LinkButton";
+import ProjectCard from "../../project/ProjectCard";
+import Container from "../../layout/Container";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+    // Para ver o loading
+    setTimeout(
+      () =>
+        fetch("http://localhost:5000/projects", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setProjects(data);
+          }),
+      100
+    );
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <div className="container">
-        <h1>Projects</h1>
+      <div className="project_container">
+        <div className="title_container">
+          <h1>Meus Projetos</h1>
+          <LinkButton to="/newproject" text="Criar projeto" />
+        </div>
+        {message && <Message type="success" msg={message} />}
+        <Container customClass="start">
+          {projects.length > 0 &&
+            projects.map((project) => (
+              <div>
+                <ProjectCard
+              
+                  name={project.name}
+                  
+                
+                  
+                />
+              </div>
+            ))}
+        </Container>
       </div>
-      <Footer />
     </div>
   );
 }
