74d6de95f21c7517487893001c30ad9d1cb663b4
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-05-29 22:51:28 -0400

    fix

--- src/pages/Home/FirstSection/index.js
+++ src/pages/Home/FirstSection/index.js
@@ -5,12 +5,18 @@ import "./style.css";
 function FirstSection() {
   return (
     <section className="first_section">
-      <div className="content">
-        <h1>Bem vindo ao WebSolutions</h1>
-        <p>
-          O seu destino confiável para serviços de desenvolvimento web
-          excepcionais, comece a gerenciar os seus projetos agora mesmo.
-        </p>
+      <div className="content_container">
+        <div className="content">
+          <h1>Bem vindo ao WebDevManager</h1>
+          <p>
+            O WebDevManager é uma plataforma online projetada para auxiliar
+            equipes e profissionais a gerenciar eficientemente projetos
+            relacionados ao desenvolvimento de sites e aplicativos web. Com
+            recursos abrangentes para infraestrutura, desenvolvimento, design e
+            planejamento, o WebDevManager oferece um ambiente colaborativo para
+            garantir o sucesso de seus projetos.
+          </p>
+        </div>
       </div>
       <div className="home_img">
         <img className="image" src={home1} alt="home"></img>
--- src/pages/Home/SecondSection/index.js
+++ src/pages/Home/SecondSection/index.js
@@ -4,43 +4,38 @@ function SecontSection() {
   return (
     <section className="second_section">
       <div className="second_content">
-        <h1>Sobre nós</h1>
+        <h1>Recursos Principais</h1>
       </div>
       <div className="grid">
         <div className="home_card">
           <div className="contend_card">
-            <h3>Serviço</h3>
             <p>
-              Oferecemos uma ampla gama de serviços de desenvolvimento web para
-              atender às suas necessidades específicas. Desde a criação de
-              websites personalizados, lojas virtuais até o desenvolvimento de
-              aplicativos web complexos, temos a experiência e o conhecimento
-              para entregar resultados impressionantes.
+              Gestão de infraestrutura para controle de servidores, ambientes de
+              desenvolvimento e bancos de dados.
             </p>
           </div>
         </div>
         <div className="home_card">
           <div className="contend_card">
-            <h3>Confiança</h3>
             <p>
-              Confie no WebSolutions como seu parceiro confiável de
-              desenvolvimento web. Entre em contato conosco hoje mesmo para
-              discutir suas ideias e iniciar a jornada para uma presença online
-              notável. Estamos ansiosos para colaborar com você e criar um site
-              que impulsione o seu sucesso!
+              Gerenciamento de design com criação de protótipos,
+              compartilhamento de feedback e iterações
             </p>
           </div>
         </div>
         <div className="home_card">
           <div className="contend_card">
-            <h3>Qualidade</h3>
             <p>
-              Nossa equipe está constantemente atualizada com as tendências do
-              setor e se esforça para fornecer soluções de ponta que o ajudarão
-              a se destacar da concorrência. Independentemente do tamanho ou
-              complexidade do seu projeto, estamos prontos para enfrentar o
-              desafio e entregar resultados excepcionais dentro do prazo e do
-              orçamento acordados.
+              Rastreamento e organização das tarefas de desenvolvimento,
+              incluindo controle de versão e integração contínua.
+            </p>
+          </div>
+        </div>
+        <div className="home_card">
+          <div className="contend_card">
+            <p>
+              Planejamento de projetos com definição de metas, prazos e alocação
+              de recursos.
             </p>
           </div>
         </div>
