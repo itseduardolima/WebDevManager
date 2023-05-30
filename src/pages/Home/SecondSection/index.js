24a8b640335ad0df1c8298791b83bbb06e755a95
eduardo.castro <eduardo.castro@conecthus.org.br>
2023-05-30 00:17:22 -0400

    fix

--- src/pages/Home/SecondSection/index.js
+++ src/pages/Home/SecondSection/index.js
@@ -9,33 +9,63 @@ function SecontSection() {
       <div className="grid">
         <div className="home_card">
           <div className="contend_card">
+            <h3>Infraestrutura</h3>
             <p>
-              Gestão de infraestrutura para controle de servidores, ambientes de
-              desenvolvimento e bancos de dados.
+              A opção de infraestrutura no WebDevManager permite que você
+              gerencie todos os aspectos técnicos relacionados ao
+              desenvolvimento web. Você pode controlar seus servidores,
+              configurar ambientes de desenvolvimento, monitorar recursos e
+              gerenciar bancos de dados de forma eficiente. Com ferramentas
+              poderosas, você pode garantir que sua infraestrutura esteja
+              configurada adequadamente para oferecer desempenho e segurança
+              ideais para seus projetos.
             </p>
           </div>
         </div>
         <div className="home_card">
           <div className="contend_card">
+            <h3>Desenvolvimento</h3>
             <p>
-              Gerenciamento de design com criação de protótipos,
-              compartilhamento de feedback e iterações
+              A seção de desenvolvimento no WebDevManager é projetada para
+              ajudar a organizar e rastrear o progresso das tarefas relacionadas
+              à codificação, programação e implementação de projetos web. Você
+              pode gerenciar suas tarefas de desenvolvimento de forma eficiente,
+              realizar controle de versão, automatizar integração contínua e
+              utilizar ferramentas de depuração para agilizar o processo de
+              desenvolvimento. Com recursos colaborativos, você pode trabalhar
+              em equipe de forma eficaz e garantir a entrega de projetos de alta
+              qualidade dentro dos prazos estabelecidos.
             </p>
           </div>
         </div>
         <div className="home_card">
           <div className="contend_card">
+            <h3>Design</h3>
             <p>
-              Rastreamento e organização das tarefas de desenvolvimento,
-              incluindo controle de versão e integração contínua.
+              A opção de design no WebDevManager oferece suporte para a gestão
+              dos aspectos visuais e de usabilidade de seus projetos web. Você
+              pode gerenciar elementos de design, criar protótipos interativos,
+              compartilhar feedback e realizar iterações para aprimorar a
+              experiência do usuário e a aparência visual do produto final. Com
+              ferramentas intuitivas e recursos de colaboração, você pode
+              colaborar com sua equipe e criar designs atraentes e eficazes para
+              seus projetos.
             </p>
           </div>
         </div>
         <div className="home_card">
           <div className="contend_card">
+            <h3>Planejamento</h3>
             <p>
-              Planejamento de projetos com definição de metas, prazos e alocação
-              de recursos.
+              A seção de planejamento no WebDevManager permite que você defina
+              metas, prazos e recursos necessários para seus projetos de
+              desenvolvimento web. Com recursos de criação de cronogramas,
+              atribuição de tarefas e acompanhamento de progresso, você pode
+              planejar de forma eficiente as etapas de seu projeto e garantir
+              que ele esteja no caminho certo. Além disso, você pode gerenciar
+              os recursos disponíveis, realizar ajustes conforme necessário e
+              manter todos os membros da equipe informados sobre o andamento do
+              projeto.
             </p>
           </div>
         </div>
